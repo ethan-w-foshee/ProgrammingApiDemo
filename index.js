5cb618d6835f74c5159f6b92801ae30ccd2c28fc
ethan.w.foshee <skyhailer2@gmail.com>
2022-12-20 15:08:10 -0600

    Code functionality established.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { API_KEY } from "./api.js";
-console.log(API_KEY)
 
 var pokeInput = document.getElementById("PokeName");
 var spritesEle = document.getElementById("sprites");
@@ -14,7 +13,6 @@ pokeInput.addEventListener("keydown", function (e) {
 
 function retrieveData() {
     pokeInput = document.getElementById("PokeName").value.toLowerCase()
-    // console.log(pokeInput)
     fetchPokeData(pokeInput)
 }
 
@@ -22,14 +20,13 @@ function fetchPokeData(pokeInput) {
     fetch(`${API_KEY}pokemon/${pokeInput}`)
         .then((res) => {
             if (!res.ok) {
-                throw new Error(`Invalid Pokemon: ${pokeInput}`);
+                window.alert(`Invalid Pokémon: ${pokeInput}`)
             }
             else {
                 return res.json();
             }
         })
         .then(data => {
-            console.log(data)
             loadImage(data)
         })
         .catch((error) => {
@@ -46,7 +43,6 @@ function fetchPokeData(pokeInput) {
             }
         })
         .then(data => {
-            console.log(data)
             loadText(data)
         })
         .catch((error) => {
@@ -65,13 +61,75 @@ function loadImage(data) {
 function loadText(data) {
     entryEle.innerHTML = ""
     let pokeEntries = data.flavor_text_entries.filter(lang => lang.language.name == "en");
-    console.log(pokeEntries)
     pokeEntries.map((results) => {
-        results.flavor_text = results.flavor_text.replaceAll("" , " ")
+        results.flavor_text = results.flavor_text.replaceAll("", " ")
         entryEle.innerHTML += `<span>${results.flavor_text}</span> <span class="gameVersion">${results.version.name}</span><br><br>`
     })
+    evoChain(data)
 }
 
 function evoChain(data) {
+    fetch(`${data.evolution_chain.url}`)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`Invalid Pokemon Chain: ${pokeInput}`);
+            }
+            else {
+                return res.json();
+            }
+        })
+        .then(data => {
+            // console.log(data)
+            evoDisplay(data)
+        })
+        .catch((error) => {
+            console.error('There has been a problem with your fetch operation:', error);
+        })
+}
 
+function evoDisplay(evoData) {
+    evoChains.innerHTML = `<span id="evoChainWords">Evolutionary Chain:</span>`
+    // console.log(`${API_KEY}pokemon/${evoData.species.name}`)
+    fetch(`${API_KEY}pokemon/${evoData.chain.species.name}`)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`Invalid Pokemon Chain: ${pokeInput}`);
+            }
+            else {
+                return res.json();
+            }
+        })
+        .then(data => {
+            evoChains.innerHTML += `<img src=${data.sprites.front_default} id="stage1" class="stages"></img>`
+        })
+        .then(evoData.chain.evolves_to.map((results) => {
+            fetch(`${API_KEY}pokemon/${results.species.name}`)
+                .then((res) => {
+                    if (!res.ok) {
+                        throw new Error(`Invalid Pokemon Chain: ${pokeInput}`);
+                    }
+                    else {
+                        return res.json();
+                    }
+                })
+                .then(data => {
+                    evoChains.innerHTML += `<img src=${data.sprites.front_default} id="stage2" class="stages"></img>`
+                })
+                .then(results.evolves_to.map((results2) => {
+                    fetch(`${API_KEY}pokemon/${results2.species.name}`)
+                        .then((res) => {
+                            if (!res.ok) {
+                                throw new Error(`Invalid Pokemon Chain: ${pokeInput}`);
+                            }
+                            else {
+                                return res.json();
+                            }
+                        })
+                        .then(data => {
+                            evoChains.innerHTML += `<img src=${data.sprites.front_default} id="stage3" class="stages"></img>`
+                        })
+                })
+                )
+        })
+        )
 }
